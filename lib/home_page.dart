694e1a753baafb5ddf2348dfae093cfca948ecb5
hachimitsuboy <39763423+hachimitsuboy@users.noreply.github.com>
2022-05-27 11:31:47 +0900

    permission_handlerを使用して権限許可をもらう機能の追加

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 import 'package:device_info_plus/device_info_plus.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -10,19 +11,17 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
   String? _deviceInfo;
 
   @override
-  void initState()  {
+  void initState() {
     DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
-
 
     Future(() async {
       deviceInfo = await _getDeviceInfo(deviceInfo);
     });
 
-    setState((){});
+    setState(() {});
     super.initState();
   }
 
@@ -48,6 +47,11 @@ class _HomePageState extends State<HomePage> {
                 Text("パッケージ名: ${snapshot.data!.packageName}"),
                 Text("バージョン: ${snapshot.data!.packageName}"),
                 Text("ビルドNo: ${snapshot.data!.buildNumber}"),
+                const SizedBox(height: 100),
+                ElevatedButton(
+                  onPressed: () => _openCamera(context),
+                  child: const Text("カメラ"),
+                ),
               ],
             ),
           );
@@ -61,15 +65,35 @@ class _HomePageState extends State<HomePage> {
   }
 
   _getDeviceInfo(DeviceInfoPlugin deviceInfo) async {
-
     AndroidDeviceInfo androidInfo = await deviceInfo.androidInfo;
     print(androidInfo.model);
 
     _deviceInfo = androidInfo.model;
   }
 
+  _openCamera(BuildContext context) async {
+    // openAppSettings();
 
+    var status = await Permission.camera.status;
+    print(status);
 
+    if (status.isDenied) {
+      _showRequestDialog(context);
+      // Either the permission was already granted before or the user just granted it.
+    }
 
+  }
 
+  void _showRequestDialog(BuildContext context) async {
+    await showDialog(context: context, builder: (context){
+      return AlertDialog(
+        title: const Text("カメラの利用には許可が必要です"),
+        content: const Text("カメラの利用を許可しますか？"),
+        actions: [
+          ElevatedButton(onPressed: () => Navigator.pop(context), child: const Text("キャンセル")),
+          ElevatedButton(onPressed: () => openAppSettings(), child: const Text("許可")),
+        ],
+      );
+    });
+  }
 }
